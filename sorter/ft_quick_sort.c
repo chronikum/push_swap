3b66581fe3d339168548e3451bd309e04205b730
Jonathan Fritz <fritz@nosc.io>
2021-09-06 08:45:23 +0200

    More stuff

--- sorter/ft_quick_sort.c
+++ sorter/ft_quick_sort.c
@@ -6,7 +6,16 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 12:32:41 by jfritz            #+#    #+#             */
-/*   Updated: 2021/08/16 12:32:41 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/04 19:32:08 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../ft_push_swap.h"
+
+/*
+**	Use quicksort algorithm to generate a valid solution
+*/
+void ft_quick_sorts_pw(t_pw **list)
+{
+
+}
