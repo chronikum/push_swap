083436fc7648e5d40cf6570fd86d70aa9ee89c61
Jonathan Fritz <fritz@nosc.io>
2021-09-18 23:47:49 +0200

    Working sort

--- helper/ft_sorter.c
+++ helper/ft_sorter.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 08:40:16 by jfritz            #+#    #+#             */
-/*   Updated: 2021/09/18 21:11:43 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/18 22:10:06 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -155,7 +155,6 @@ void	ft_sequential_push(t_pw **arr, int s, int remainer)
 
 	beginning1 = ft_get_biggest((*(*arr)->b))->value;
 	beginning2 = ft_get_biggest((*(*arr)->b))->value;
-	// Smaller numbers
 	while ((*(*arr)->b))
 	{
 		position = ft_find_position((*(*arr)->b), ft_get_biggest((*(*arr)->b))->value);
@@ -173,7 +172,6 @@ void	ft_sequential_push(t_pw **arr, int s, int remainer)
 		ft_rra(arr);
 	}
 	ft_ra(arr);
-	// Bigger numbers
 	while ((*(*arr)->a)->value != beginning2)
 		ft_pb(arr);
 	while ((*(*arr)->b))
@@ -188,106 +186,9 @@ void	ft_sequential_push(t_pw **arr, int s, int remainer)
 		}
 		ft_pa(arr);
 	}
-
-	// OLD
-	// while (total <= (*arr)->count && remainer)
-	// {
-	// 	last_iter = 0;
-	// 	while ((ft_lstvalsize((*(*arr)->b)) < s) && (*(*arr)->a))
-	// 	{
-	// 		total++;
-	// 		if ((*(*arr)->b))
-	// 		{
-	// 			sp = ft_find_order((*(*arr)->b), (*(*arr)->a)->value);
-	// 			while (ih != (sp + 1))
-	// 			{
-	// 				ft_rb(arr);
-	// 				ih++;
-	// 			}
-	// 		}
-	// 		ft_pb(arr);
-	// 		last_iter++;
-	// 		already++;
-	// 		while (ih != 0)
-	// 		{
-	// 			ft_rrb(arr);
-	// 			ih--;
-	// 		}
-	// 	}
-	// 	if (!ft_check_stack_sorted((*(*arr)->b)))
-	// 		ft_rb(arr);
-	// 	if (!ft_check_stack_sorted((*(*arr)->b)))
-	// 		ft_putstr_fd("ERROR! STACK IS NOT SORTED", 1);
-	// 	while ((ft_lstvalsize((*(*arr)->b)) != 0))
-	// 		ft_pa(arr);
-	// 	while (already != 0)
-	// 	{
-	// 		already--;
-	// 		ft_ra(arr);
-	// 	}
-	// }
-	// while ((ft_lstvalsize((*(*arr)->a)) > last_iter))
-	// 	ft_pb(arr);
-	// if ((ft_get_biggest((*(*arr)->a)) != (*(*arr)->a)) && !(ft_check_stack_sorted((*(*arr)->a))))
-	// 	ft_pb(arr);
-	// OLD
-
-	// while (ft_lstvalsize((*(*arr)->b)) != 0)
-	// {
-	// 	ih = 0;
-	// 	sp = ft_find_order2((*(*arr)->b), (*(*arr)->a)->index);
-	// 	while (ih != (sp + 1) && sp != 0)
-	// 	{
-	// 		ft_ra(arr);
-	// 		ih++;
-	// 	}
-	// 	ft_pa(arr);
-	// }
-
-	// while (ft_lstvalsize((*(*arr)->b)) != 0)
-	// {
-	// 	ih = 0;
-	// 	sp = ft_find_order((*(*arr)->b), (*(*arr)->a)->value);
-	// 	while (ih != (sp + 1) && sp != 0)
-	// 	{
-	// 		ft_ra(arr);
-	// 		ih++;
-	// 	}
-	// 	ft_pa(arr);
-	// }
 }
 
 void    ft_sorter(t_pw **pw)
 {
-	int	splitter;
-	int	total;
-	int remainer;
-
-	total = 0;
-	splitter = 10;
-	remainer = 10;
-	if (((*pw)->count) >= 100)
-	{
-		splitter = 25;
-		remainer = 25;
-	}
-	if (((*pw)->count) >= 200)
-	{
-		splitter = 10;
-		remainer = 10;
-	}
-	if (((*pw)->count) > 350)
-	{
-		splitter = 30;
-		remainer = 30;
-	}
-	if (((*pw)->count) >= 500)
-	{
-		splitter = 25;
-		remainer = 10;
-	}
-	if ((*pw)->count <= 50)
-		ft_below_50((*pw));
-	else
-		ft_sequential_push(pw, splitter, remainer);
+	ft_sequential_push(pw);
 }
