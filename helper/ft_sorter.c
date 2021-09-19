316f510a7b0ad3a9b919f2af61a1b7eb9cc76c98
Jonathan Fritz <fritz@nosc.io>
2021-09-19 09:45:39 +0200

    Sub 250 norm conform now

--- helper/ft_sorter.c
+++ helper/ft_sorter.c
@@ -6,175 +6,21 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 08:40:16 by jfritz            #+#    #+#             */
-/*   Updated: 2021/09/19 09:36:08 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/19 09:45:25 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../ft_push_swap.h"
 
-/*
-**	Returns the index the value should be in
-*/
-// static int	ft_find_order(t_val *l, int x)
-// {
-// 	int counter;
-// 	t_val *tmp;
-
-// 	counter = 0;
-// 	tmp = l;
-// 	if (ft_get_smallest(l)->value > x)
-// 		return (0);
-// 	if (ft_get_biggest(l)->value < x)
-// 		return (ft_lstvalsize(l));
-// 	while (tmp)
-// 	{
-// 		if (tmp->next)
-// 		{
-// 			if (tmp->value < x && tmp->next->value > x)
-// 				return (counter);
-// 		}
-// 		counter++;
-// 		tmp = tmp->next;
-// 	}
-// 	return (counter - 1);
-// }
-
-// static int	ft_find_order2(t_val *l, int x)
-// {
-// 	int counter;
-// 	t_val *tmp;
-
-// 	counter = 0;
-// 	tmp = l;
-// 	if (ft_get_smallest(l)->value > x)
-// 		return (0);
-// 	if (ft_get_biggest(l)->value < x)
-// 		return (ft_lstvalsize(l));
-// 	while (tmp)
-// 	{
-// 		if (tmp->next)
-// 		{
-// 			if (tmp->index < x && tmp->next->index > x)
-// 				return (counter);
-// 		}
-// 		counter++;
-// 		tmp = tmp->next;
-// 	}
-// 	return (counter - 1);
-// }
-
-// static int	ft_find_order2(t_val *l, int x)
-// {
-// 	int counter;
-// 	t_val *tmp;
-
-// 	counter = 0;
-// 	tmp = l;
-// 	if (ft_get_smallest(l)->value > x)
-// 		return (0);
-// 	if (ft_get_biggest(l)->value < x)
-// 		return (ft_lstvalsize(l));
-// 	while (tmp)
-// 	{
-// 		if (tmp->next)
-// 		{
-// 			if (tmp->value < x && tmp->next->value > x)
-// 				return (counter);
-// 		}
-// 		counter++;
-// 		tmp = tmp->next;
-// 	}
-// 	return (counter - 1);
-// }
-
-// static int	ft_find_order2(t_val *l, int x)
-// {
-// 	int counter;
-// 	t_val *smallest;
-// 	t_val *biggest;
-// 	int *array;
-
-// 	counter = 0;
-// 	if (!l)
-// 		return (0);
-// 	smallest = ft_get_smallest(l);
-// 	biggest = ft_get_biggest(l);
-// 	array = malloc(sizeof(int) * ft_lstvalsize(l));
-// 	ft_stoip(l, &array);
-// 	ft_bubble_sort(array, ft_lstvalsize(l));
-// 	if (smallest->value > x)
-// 		return (ft_find_position(l, smallest->value));
-// 	if (biggest->value < x)
-// 		return (ft_find_position(l, biggest->value));
-// 	while (counter < ft_lstvalsize(l) && counter >= 0)
-// 	{
-// 		if (array[counter - 1] < x && array[counter] > x)
-// 			break;
-// 		counter++;
-// 	}
-// 	return (ft_find_position(l, array[counter - 1]));
-
-// 	// if (smallest->value > x)
-// 	// 	return (0);
-// 	// if (biggest->value < x)
-// 	// 	return (ft_lstvalsize(l));
-// 	// while (tmp)
-// 	// {
-// 	// 	if (tmp->next)
-// 	// 	{
-// 	// 		if (tmp->value < x && tmp->next->value > x)
-// 	// 			return (counter);
-// 	// 	}
-// 	// 	counter++;
-// 	// 	tmp = tmp->next;
-// 	// }
-// 	return (counter);
-// }
-
-void	ft_sequential_push(t_pw **arr)
+void	ft_half_sort(t_pw **arr)
 {
-	int	total;
-	int	beginning1;
-	int	beginning2;
-	int	position;
+	int		position;
+	t_val	*biggest;
 
-	beginning1 = 0;
-	total = 0;
-	position = 0;
-	while ((total <= (*arr)->count))
-	{
-		if ((*(*arr)->a)->index > ((*arr)->count / 2))
-			ft_pb(arr);
-		else
-			ft_ra(arr);
-		total++;
-	}
-
-	beginning1 = ft_get_biggest((*(*arr)->b))->value;
-	beginning2 = ft_get_biggest((*(*arr)->b))->value;
 	while ((*(*arr)->b))
 	{
-		position = ft_find_position((*(*arr)->b), ft_get_biggest((*(*arr)->b))->value);
-		while (ft_get_biggest((*(*arr)->b))->value != (*(*arr)->b)->value)
-		{
-			if (position < ft_lstvalsize((*(*arr)->b)) / 2)
-				ft_rb(arr);
-			else
-				ft_rrb(arr);
-		}
-		ft_pa(arr);
-	}
-	while (beginning1 != (*(*arr)->a)->value)
-	{
-		ft_rra(arr);
-	}
-	ft_ra(arr);
-	while ((*(*arr)->a)->value != beginning2)
-		ft_pb(arr);
-	while ((*(*arr)->b))
-	{
-		position = ft_find_position((*(*arr)->b), ft_get_biggest((*(*arr)->b))->value);
+		biggest = ft_get_biggest((*(*arr)->b));
+		position = ft_find_position((*(*arr)->b), biggest->value);
 		while (ft_get_biggest((*(*arr)->b))->value != (*(*arr)->b)->value)
 		{
 			if (position < ft_lstvalsize((*(*arr)->b)) / 2)
@@ -186,7 +32,39 @@ void	ft_sequential_push(t_pw **arr)
 	}
 }
 
-void    ft_sorter(t_pw **pw)
+/*
+**	Halfs stack a and sorts them both
+**	Appropiate for 80-250
+*/
+void	ft_sequential_push(t_pw **arr)
+{
+	int	total;
+	int	beginning1;
+	int	beginning2;
+
+	total = 0;
+	while ((total <= (*arr)->count))
+	{
+		if ((*(*arr)->a)->index > ((*arr)->count / 2))
+			ft_pb(arr);
+		else
+			ft_ra(arr);
+		total++;
+	}
+	beginning1 = ft_get_biggest((*(*arr)->b))->value;
+	beginning2 = ft_get_biggest((*(*arr)->b))->value;
+	ft_half_sort(arr);
+	while (beginning1 != (*(*arr)->a)->value)
+	{
+		ft_rra(arr);
+	}
+	ft_ra(arr);
+	while ((*(*arr)->a)->value != beginning2)
+		ft_pb(arr);
+	ft_half_sort(arr);
+}
+
+void	ft_sorter(t_pw **pw)
 {
 	ft_sequential_push(pw);
 }
