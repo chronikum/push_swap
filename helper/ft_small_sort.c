d0ca1b58a680ef49d99547f58a801972761dddc7
Jonathan Fritz <fritz@nosc.io>
2021-09-19 15:37:31 +0200

    ft small sort is now norm conform

--- helper/ft_small_sort.c
+++ helper/ft_small_sort.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 12:03:44 by jfritz            #+#    #+#             */
-/*   Updated: 2021/09/19 15:34:57 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/19 15:37:19 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static int	ft_check_last_two(t_pw **arr)
 {
-	if (ft_lstvallast((*(*arr)->a)) != ft_get_smallest((*(*arr)->a)) &&
-	((*(*arr)->a)) == ft_get_biggest((*(*arr)->a)))
+	if (ft_lstvallast((*(*arr)->a)) != ft_get_smallest((*(*arr)->a))
+		&& ((*(*arr)->a)) == ft_get_biggest((*(*arr)->a)))
 	{
 		ft_ra(arr);
 		return (1);
@@ -26,13 +26,13 @@ static int	ft_check_last_two(t_pw **arr)
 static	void	ft_sub_three(t_pw **arr)
 {
 	if (ft_check_three_rev_sorted(arr))
-		return;
+		return ;
 	if (ft_check_last_two(arr))
-		return;
+		return ;
 	if ((*(*arr)->a)->index > (*(*arr)->a)->next->index)
 	{
 		ft_sa(arr);
-		return;
+		return ;
 	}
 	if (ft_get_biggest((*(*arr)->a)) == (*(*arr)->a))
 		ft_rra(arr);
