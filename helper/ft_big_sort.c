a8c9c826c7d9b3b9c5fe67892a1ddb2ad29e43a9
Jonathan Fritz <fritz@nosc.io>
2021-09-20 14:22:44 +0200

    Big sort is now norm conform

--- helper/ft_big_sort.c
+++ helper/ft_big_sort.c
@@ -6,27 +6,46 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 16:59:10 by jfritz            #+#    #+#             */
-/*   Updated: 2021/09/20 13:16:31 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/20 14:22:29 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_push_swap.h"
 
-void	ft_pre_sort(t_pw **arr)
+/*
+**	Divides the given stack by amount and pre-determined index,
+**	after that we check where each value should be and then
+**	we rotate stack to the smallest b-previous number and sort it
+**	on top starting with the biggest in stack b.
+*/
+void	ft_sort_part(t_pw **arr, int b1, int b2)
 {
-	int total;
+	int	d;
+	int	n;
 
-	total = 0;
-	while (total < (*arr)->count)
+	d = 3;
+	n = 6;
+	while (d != -1)
 	{
+		ft_split_quarter(arr, n, d, b2);
+		b1 = ft_get_biggest((*(*arr)->a))->index;
+		b2 = ft_get_smallest((*(*arr)->b))->index;
+		ft_half_sort(arr);
+		while (b1 != (*(*arr)->a)->index)
+			ft_rra(arr);
 		ft_ra(arr);
-		total++;
+		d--;
 	}
 }
 
 /*
-**	Quarters stack a and sorts them both
-**	Appropiate for over 250 and counting
+**	Sorts stack a using partitions
+**	Creates by default 6 partitions
+**	and sorts them seperatelz on the top
+**	of the existing stack. Uses indexing
+**	of values to determine stacks of different
+**	index values, so we can start with the highest indexes
+**	at the beginning.
 */
 void	ft_bigger_push(t_pw **arr)
 {
@@ -40,7 +59,6 @@ void	ft_bigger_push(t_pw **arr)
 	while (beginning1 != (*(*arr)->a)->index)
 		ft_ra(arr);
 	ft_ra(arr);
-
 	ft_split_quarter(arr, 6, 4, beginning2);
 	beginning1 = ft_get_biggest((*(*arr)->a))->index;
 	beginning2 = ft_get_smallest((*(*arr)->b))->index;
@@ -48,58 +66,5 @@ void	ft_bigger_push(t_pw **arr)
 	while (beginning1 != (*(*arr)->a)->index)
 		ft_ra(arr);
 	ft_ra(arr);
-
-	ft_split_quarter(arr, 6, 3, beginning2);
-	beginning1 = ft_get_biggest((*(*arr)->a))->index;
-	beginning2 = ft_get_smallest((*(*arr)->b))->index;
-	ft_half_sort(arr);
-	while (beginning1 != (*(*arr)->a)->index)
-		ft_rra(arr);
-	ft_ra(arr);
-
-	ft_split_quarter(arr, 6, 2, beginning2);
-	beginning1 = ft_get_biggest((*(*arr)->a))->index;
-	beginning2 = ft_get_smallest((*(*arr)->b))->index;
-	ft_half_sort(arr);
-	while (beginning1 != (*(*arr)->a)->index)
-		ft_rra(arr);
-	ft_ra(arr);
-
-	ft_split_quarter(arr, 6, 1, beginning2);
-	beginning1 = ft_get_biggest((*(*arr)->a))->index;
-	beginning2 = ft_get_smallest((*(*arr)->b))->index;
-	ft_half_sort(arr);
-	while (beginning1 != (*(*arr)->a)->index)
-		ft_rra(arr);
-	ft_ra(arr);
-
-	ft_split_quarter(arr, 6, 0, beginning2);
-	beginning1 = ft_get_biggest((*(*arr)->a))->index;
-	beginning2 = ft_get_smallest((*(*arr)->b))->index;
-	ft_half_sort(arr);
-	while (beginning1 != (*(*arr)->a)->index)
-		ft_rra(arr);
-	ft_ra(arr);
-
-
-
-	// ft_split_quarter(arr, 5, 1, beginning2);
-	// ft_half_sort(arr);
-	// ft_ra(arr);
-
-	// ft_ra(arr);
-	// beginning1 = ft_get_smallest((*(*arr)->a))->index;
-	// beginning2 = ft_get_smallest((*(*arr)->a))->index;
-	// while (beginning1 != (*(*arr)->a)->index)
-	// 	ft_ra(arr);
-	// ft_half_sort(arr);
-	// ft_split_quarter(arr, 1, beginning2);
-	// ft_half_sort(arr);
-	// beginning1 = ft_get_biggest((*(*arr)->a))->index;
-	// beginning2 = (*(*arr)->a)->index;
-	// while (beginning1 != ft_lstvallast((*(*arr)->a))->index)
-	// 	ft_ra(arr);
-	// while (beginning2 != (*(*arr)->a)->index)
-	// 	ft_pb(arr);
-	// ft_half_sort(arr);
+	ft_sort_part(arr, beginning1, beginning2);
 }
