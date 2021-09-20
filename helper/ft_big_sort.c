1f255322a7154c22acc4e5fd0dc2e5634860c08e
Jonathan Fritz <fritz@nosc.io>
2021-09-20 12:33:10 +0200

    Bigger sort is way better than the sorting algorithm before

--- helper/ft_big_sort.c
+++ helper/ft_big_sort.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 16:59:10 by jfritz            #+#    #+#             */
-/*   Updated: 2021/09/20 11:54:39 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/20 12:28:18 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ void	ft_bigger_push(t_pw **arr)
 	int	beginning1;
 	int	beginning2;
 
-	// ft_pre_sort(arr);
 	ft_split_quarter(arr, 3, -1000);
 	beginning1 = ft_get_biggest((*(*arr)->b))->index;
 	beginning2 = ft_get_smallest((*(*arr)->b))->index;
@@ -41,19 +40,39 @@ void	ft_bigger_push(t_pw **arr)
 	while (beginning1 != (*(*arr)->a)->index)
 		ft_ra(arr);
 	ft_ra(arr);
+
 	ft_split_quarter(arr, 2, beginning2);
-	ft_ra(arr);
-	beginning1 = ft_get_smallest((*(*arr)->a))->index;
+	beginning1 = ft_get_biggest((*(*arr)->a))->index;
+	beginning2 = ft_get_smallest((*(*arr)->b))->index;
+	ft_half_sort(arr);
 	while (beginning1 != (*(*arr)->a)->index)
 		ft_ra(arr);
-	ft_half_sort(arr);
-	ft_split_quarter(arr, 1, -1);
-	ft_half_sort(arr);
+	ft_ra(arr);
+
+	ft_split_quarter(arr, 1, beginning2);
 	beginning1 = ft_get_biggest((*(*arr)->a))->index;
-	beginning2 = (*(*arr)->a)->index;
-	while (beginning1 != ft_lstvallast((*(*arr)->a))->index)
-		ft_ra(arr);
-	while (beginning2 != (*(*arr)->a)->index)
-		ft_pb(arr);
+	beginning2 = ft_get_smallest((*(*arr)->b))->index;
 	ft_half_sort(arr);
+	while (beginning1 != (*(*arr)->a)->index)
+		ft_rra(arr);
+	ft_ra(arr);
+	ft_split_quarter(arr, 0, beginning2);
+	ft_half_sort(arr);
+	ft_rra(arr);
+
+	// ft_ra(arr);
+	// beginning1 = ft_get_smallest((*(*arr)->a))->index;
+	// beginning2 = ft_get_smallest((*(*arr)->a))->index;
+	// while (beginning1 != (*(*arr)->a)->index)
+	// 	ft_ra(arr);
+	// ft_half_sort(arr);
+	// ft_split_quarter(arr, 1, beginning2);
+	// ft_half_sort(arr);
+	// beginning1 = ft_get_biggest((*(*arr)->a))->index;
+	// beginning2 = (*(*arr)->a)->index;
+	// while (beginning1 != ft_lstvallast((*(*arr)->a))->index)
+	// 	ft_ra(arr);
+	// while (beginning2 != (*(*arr)->a)->index)
+	// 	ft_pb(arr);
+	// ft_half_sort(arr);
 }
--- helper/ft_sorter.c
+++ helper/ft_sorter.c
@@ -6,51 +6,51 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 08:40:16 by jfritz            #+#    #+#             */
-/*   Updated: 2021/09/20 11:51:30 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/20 12:32:38 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_push_swap.h"
 
-/*
-**	Halfs stack a and sorts them both
-**	Appropiate for over 250 and counting
-*/
-static void	ft_big_push(t_pw **arr)
-{
-	int	beginning1;
-	int	beginning2;
+// /*
+// **	Halfs stack a and sorts them both
+// **	Appropiate for over 250 and counting
+// */
+// static void	ft_big_push(t_pw **arr)
+// {
+// 	int	beginning1;
+// 	int	beginning2;
 
-	ft_split_half(arr);
-	beginning1 = ft_get_biggest((*(*arr)->b))->value;
-	beginning2 = ft_get_smallest((*(*arr)->b))->value;
-	ft_half_sort(arr);
-	while (beginning1 != (*(*arr)->a)->value)
-		ft_ra(arr);
-	ft_ra(arr);
-	while ((*(*arr)->a)->value != beginning2)
-	{
-		if ((*(*arr)->a)->index > (((*arr)->count / 2) / 2))
-			ft_pb(arr);
-		else
-			ft_ra(arr);
-	}
-	beginning1 = ft_get_biggest((*(*arr)->a))->value;
-	ft_half_sort(arr);
-	while (ft_lstvallast((*(*arr)->a))->value != beginning1)
-	{
-		ft_rra(arr);
-		ft_pb(arr);
-	}
-	ft_half_sort(arr);
-}
+// 	ft_split_half(arr);
+// 	beginning1 = ft_get_biggest((*(*arr)->b))->value;
+// 	beginning2 = ft_get_smallest((*(*arr)->b))->value;
+// 	ft_half_sort(arr);
+// 	while (beginning1 != (*(*arr)->a)->value)
+// 		ft_ra(arr);
+// 	ft_ra(arr);
+// 	while ((*(*arr)->a)->value != beginning2)
+// 	{
+// 		if ((*(*arr)->a)->index > (((*arr)->count / 2) / 2))
+// 			ft_pb(arr);
+// 		else
+// 			ft_ra(arr);
+// 	}
+// 	beginning1 = ft_get_biggest((*(*arr)->a))->value;
+// 	ft_half_sort(arr);
+// 	while (ft_lstvallast((*(*arr)->a))->value != beginning1)
+// 	{
+// 		ft_rra(arr);
+// 		ft_pb(arr);
+// 	}
+// 	ft_half_sort(arr);
+// }
 
 void	ft_sorter(t_pw **pw)
 {
+	ft_bigger_push(pw);
+	return;
 	if ((*pw)->count > 1 && (*pw)->count < 11)
 		ft_small_sort(pw);
-	else if ((*pw)->count > 10 && (*pw)->count < 250)
-		ft_big_push(pw);
 	else
 		ft_bigger_push(pw);
 }
