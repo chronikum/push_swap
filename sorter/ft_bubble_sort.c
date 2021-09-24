43ed6cbc428b19c4d999223177c95ccc8c2e3830
Jonathan Fritz <fritz@nosc.io>
2021-09-24 10:31:28 +0200

    Adding docu

--- sorter/ft_bubble_sort.c
+++ sorter/ft_bubble_sort.c
@@ -6,12 +6,15 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 12:32:41 by jfritz            #+#    #+#             */
-/*   Updated: 2021/09/20 17:37:05 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/09/24 10:28:12 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_push_swap.h"
 
+/*
+**	Swap ints
+*/
 static void	ft_swap_ints(int *x_p, int *y_p)
 {
 	int	temp;
@@ -21,6 +24,10 @@ static void	ft_swap_ints(int *x_p, int *y_p)
 	(*y_p) = temp;
 }
 
+/*
+**	By using bubble sort we can index all values and add additional
+**	information to the element in the stack.
+*/
 void	ft_bubble_sort(int arr[], int n)
 {
 	int	i;
